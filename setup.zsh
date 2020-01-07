63ebce6d1963f6f8bdb2c0984227f3de7d137bf9
naka-gawa <tomoaki-nakagawa@c-fo.com>
2020-01-07 14:36:19 +0900

    setup ci

--- setup.zsh
+++ setup.zsh
@@ -1,4 +1,11 @@
 #!/bin/zsh -e
 
-pwd
+DOTPATH=$(pwd)
+ln -s $DOTPATH/zsh/.zshrc ~/.zshrc
 
+mkdir ~/.zsh.d
+for FILE in $(find $DOTPATH/zsh/.zsh.d);
+do
+  FILENAME=$(basename $FILE)
+  ln -s $DOTPATH/zsh/.zsh.d/$FILENAME ~/.zsh.d
+done
